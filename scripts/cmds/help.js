0f3aa02cce9e69e4e60bf4213abb3e2111ca34ca
GabYu951 <160740489+GabYu951@users.noreply.github.com>
2024-03-31 22:51:58 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ Gojo | ☣️ ]";
+const doNotDelete = "[ Kaizoku | 🌀 ]";
 /** 
 * @author NTKhang
 * @author: do not delete it
