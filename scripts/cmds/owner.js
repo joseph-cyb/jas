f97c54c59497171c20a68aeb3ae75446bbc808e7
GabYu951 <160740489+GabYu951@users.noreply.github.com>
2024-04-23 22:39:53 +0800

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -24,7 +24,7 @@ config: {
       nick: '𝗚𝗜𝗬𝗨'
     };
 
-    const bold = 'https://i.imgur.com/DDO686J.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
+    const bold = 'https://tinyurl.com/263qodt3'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
 
     const tmpFolderPath = path.join(__dirname, 'tmp');
 
