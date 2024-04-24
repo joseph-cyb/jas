5c9b7024a1a93e5c12d0f8f6b4abbf9f3156a047
GabYu951 <160740489+GabYu951@users.noreply.github.com>
2024-04-24 10:33:46 +0800

    Update cmds

---
+++ scripts/cmds/Gabyu.js
@@ -0,0 +1,20 @@
+module.exports = {
+	config: {
+			name: "GabYu",
+			version: "1.0",
+			author: "GabYu",
+			countDown: 5,
+			role: 0,
+			shortDescription: "sarcasm",
+			longDescription: "sarcasm",
+			category: "reply",
+	},
+onStart: async function(){}, 
+onChat: async function({
+	event,
+	message,
+	getLang
+}) {
+	if (event.body && event.body.toLowerCase() == "@Gab Yu") return message.reply("Busy Owner ko");
+}
+};
