ae0f3dc40c3b4b7c43fe0f498b3eba4768c81679
Nonchalant557 <trixiezruc@gmail.com>
2024-06-11 12:34:57 +0800

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -17,7 +17,7 @@ module.exports = {
  body: `
 Yo, my prefix is [ 𓆩 / 𓆪 ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
-➥ /help [number of page] -> see commands
+➥ /gojo [prompt] -> talk to gojo
 ➥ /sim [message] -> talk to bot
 ➥ /callad [message] -> report any problem encountered
 ➥ /help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/profile.php?id=100079114908948`,
